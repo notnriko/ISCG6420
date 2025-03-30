cdd02acae0a48b1474171b3775e7a8c478079ab5
notnriko <enricomiguellorenzo@gmail.com>
2025-03-31 11:26:36 +1300

    Week 5

---
+++ Week 5/formPaging.js
@@ -0,0 +1,7 @@
+const formPage1 = document.getElementById("form-page1");
+const formPage2 = document.getElementById("form-page2");
+const formPage3 = document.getElementById("form-page3");
+
+const progressBar = document.querySelector("progress");
+
+const summaryName = document.getElementById
